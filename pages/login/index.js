1f7294654df9a2eef4b9c90203154a19e96c9152
bayuik <bayuindrakusuma05@gmail.com>
2022-06-28 11:46:40 +0700

    api integration login and register

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,59 +1,56 @@
-import { Col, Row, Image, Form, Button } from "react-bootstrap";
+import { Col, Row, Form, Button } from "react-bootstrap";
 import Link from "next/link";
 import { LoginImage } from "../../components";
 import React from "react";
 import { LoginUser } from "../../lib/auth";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
-class Login extends React.Component {
-  state = {
-    email: "",
-    password: "",
+const Login = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = async (data) => {
+    const { email, password } = data;
+    const res = await axios.post("https://api-secondhand-fsw.herokuapp.com/login", {
+      email,
+      password,
+    });
+    if (res.data.status == "success") {
+      alert("Successfully Logged In");
+    } else {
+      alert(res.data.message);
+    }
   };
-
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    const { email, password } = this.state;
-
-    event.preventDefault();
-    LoginUser(email, password);
-  };
-
-  render() {
-    return (
-      <Row>
-        <LoginImage />
-        <Col md={6} className="my-auto">
-          <div className="mx-auto w-75">
-            <h1 className="fw-bold">Masuk</h1>
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Group controlId="email" className="mt-3">
-                <Form.Label>Email</Form.Label>
-                <Form.Control onChange={this.handleChange} name="email" type="email" placeholder="Contoh: johndee@gmail.com" className="custom-rounded p-2" />
-              </Form.Group>
-              <Form.Group controlId="password" className="mt-3">
-                <Form.Label>Password</Form.Label>
-                <Form.Control onChange={this.handleChange} name="password" type="password" placeholder="Masukkan password" className="custom-rounded p-2" />
-              </Form.Group>
-              <div className="d-grid gap-2 mt-4">
-                <button className="btn text-white purple-bg custom-rounded p-2" type="submit">
-                  Masuk
-                </button>
-              </div>
-            </Form>
-            <div className="text-center mt-4">
-              <p className="d-inline">Belum punya akun? </p>
-              <Link href="/register">
-                <a className="purple-text fw-bold text-decoration-none">Daftar di sini</a>
-              </Link>
+  return (
+    <Row>
+      <LoginImage />
+      <Col md={6} className="my-auto">
+        <div className="mx-auto w-75">
+          <h1 className="fw-bold">Masuk</h1>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group controlId="email" className="mt-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control name="email" type="email" placeholder="Contoh: johndee@gmail.com" className="custom-rounded p-2" {...register("email")} />
+            </Form.Group>
+            <Form.Group controlId="password" className="mt-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control name="password" type="password" placeholder="Masukkan password" className="custom-rounded p-2" {...register("password")} />
+            </Form.Group>
+            <div className="d-grid gap-2 mt-4">
+              <button className="btn text-white purple-bg custom-rounded p-2" type="submit">
+                Masuk
+              </button>
             </div>
+          </Form>
+          <div className="text-center mt-4">
+            <p className="d-inline">Belum punya akun? </p>
+            <Link href="/register">
+              <a className="purple-text fw-bold text-decoration-none">Daftar di sini</a>
+            </Link>
           </div>
-        </Col>
-      </Row>
-    );
-  }
-}
+        </div>
+      </Col>
+    </Row>
+  );
+};
 
 export default Login;
--- pages/register/index.js
+++ pages/register/index.js
@@ -1,19 +1,25 @@
-import { Col, Row, Form, Button } from "react-bootstrap";
+import { Col, Row, Form } from "react-bootstrap";
 import Link from "next/link";
 import { LoginImage } from "../../components";
 import axios from "axios";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-  axios.post('/http://localhost:8000/register', {
-    firstName: 'Fred',
-    lastName: 'Flintstone'
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  const {register, handleSubmit, errors } = useForm();
+  const onSubmit = async (data) => {
+    const {name, email, password } = data;
+    const res = await axios.post("https://api-secondhand-fsw.herokuapp.com/register", {
+      name,
+      email,
+      password,
+    });
+    if (res.data.status == "success") {
+      alert("Successfully Registered");
+      window.location.href = "/login";
+    } else {
+      alert(res.data.message);
+    }
+  }
 
   return (
     <Row>
@@ -21,21 +27,21 @@ const Register = () => {
       <Col md={6} className="my-auto">
         <div className="mx-auto w-75">
           <h1 className="fw-bold">Daftar</h1>
-          <Form>
+          <Form onSubmit={handleSubmit(onSubmit)} >
             <Form.Group controlId="name" className="mt-3">
               <Form.Label>Nama</Form.Label>
-              <Form.Control type="text" placeholder="Nama Lengkap" className="custom-rounded p-2" />
+              <Form.Control type="text" placeholder="Nama Lengkap" className="custom-rounded p-2" {...register("name")} />
             </Form.Group>
             <Form.Group controlId="email" className="mt-3">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Contoh: johndee@gmail.com" className="custom-rounded p-2" />
+              <Form.Control type="email" placeholder="Contoh: johndee@gmail.com" className="custom-rounded p-2" {...register("email")} />
             </Form.Group>
             <Form.Group controlId="password" className="mt-3">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Masukkan password" className="custom-rounded p-2" />
+              <Form.Control type="password" placeholder="Masukkan password" className="custom-rounded p-2" {...register("password")} />
             </Form.Group>
             <div className="d-grid gap-2 mt-4">
-              <button className="btn text-white purple-bg custom-rounded p-2" type="button">
+              <button className="btn text-white purple-bg custom-rounded p-2" type="submit">
                 Daftar
               </button>
             </div>
