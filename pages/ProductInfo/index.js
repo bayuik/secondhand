b5b5b216bf4aa10413ad6ea714b2169c868a163a
IlmanNawali <79490380+IlmanNawali@users.noreply.github.com>
2022-06-29 20:46:30 +0700

    Update index.js

--- pages/ProductInfo/index.js
+++ pages/ProductInfo/index.js
@@ -8,7 +8,7 @@ import {
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { useState, useEffect } from "react";
-function ProductInfo({id}) {
+function Product({id}) {
   const [product, setProducts] = useState([])
 
   const getProducts = async () => {
@@ -24,7 +24,6 @@ function ProductInfo({id}) {
   useEffect(() => {
       getProducts();
   }, [])
-  
   return (
     <Row>
         <div className="invisible-content">
@@ -48,84 +47,74 @@ function ProductInfo({id}) {
         </div>
 
       <div className="spacing mx-auto">
-        <Container >
-        {
-                product.map(products => {
-                    return (
-                      <Row className="justify-content-md-center">
-                      <Col sm={6}>
-                      <Carousel className="product-img">
-                              <div>
-                                <Image src="/1.png" alt="image1" className="product_img rounded-3"></Image>
-                              </div>
-                              <div>
-                                <Image src="/1.png" alt="image2" className="product_img rounded-3"></Image>
-                              </div>
-                              <div>
-                                <Image src="/1.png" alt="image3" className="product_img rounded-3"></Image>
-                              </div>
-                          </Carousel>
-                      </Col>
-          
-                      
-                      <Col sm={4}>
-                        <Card className="card-product">
-                          <Card.Body>
-                            <Card.Title>{products.product_name}</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Aksesoris</Card.Subtitle>
-                            <Card.Text>
-                              Rp. 250.000
-                            </Card.Text>
-                            <div className="d-grid gap-2 mt-4">
-                            <Button className="text-white purple-bg custom-rounded p-2" type="button" href="/login">Saya tertarik dan ingin nego</Button>
-                            </div>
-                          </Card.Body>
-                        </Card>
-          
-                        <Card className="mt-3 card-size card-product">
-                          <Card.Body>
-                            <div class="row">
-                              <div class="col-3">
-                                <Image src="/penjual1.png" alt="penjual1"className="seller_img rounded-3"></Image>
-                              </div>
-                              <div class="col">
-                                <Card.Title>Nama Penjual</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted kota">Kota</Card.Subtitle>
-                              </div>
-                            </div>
-                          </Card.Body>
-                        </Card>
-                      </Col>
-                    </Row>
-                    
+      <Container >
+          <Row className="justify-content-md-center">
+            <Col sm={6}>
+            <Carousel className="product-img">
+                    <div>
+                      <Image src="/1.png" alt="image1" className="product_img rounded-3"></Image>
+                    </div>
+                    <div>
+                      <Image src="/1.png" alt="image2" className="product_img rounded-3"></Image>
+                    </div>
+                    <div>
+                      <Image src="/1.png" alt="image3" className="product_img rounded-3"></Image>
+                    </div>
+                </Carousel>
+            </Col>
+            <Col sm={4}>
+              <Card className="card-product">
+                <Card.Body>
+                  <Card.Title>Jam Tangan Casio</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Aksesoris</Card.Subtitle>
+                  <Card.Text>
+                    Rp. 250.000
+                  </Card.Text>
+                  <div className="d-grid gap-2 mt-4">
+                  <Button className="text-white purple-bg custom-rounded p-2" type="button">Saya tertarik dan ingin nego</Button>
+                  </div>
+                </Card.Body>
+              </Card>
 
-                    )
-                })
-            }
-                              <Row className="justify-content-md-center mt-3">
-                      <Col sm={6}>
-                        <Card className="card-description">
-                          <Card.Body>
-                            <Card.Title>Deskripsi</Card.Title>
-                            <Card.Text className="deskripsi">
-                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </Card.Text>
-                            <Card.Text className="deskripsi">
-                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </Card.Text>
-                          </Card.Body>
-                        </Card>
-                      </Col>
-                      <Col sm={4}></Col>
-                    </Row>
+              <Card className="mt-3 card-size card-product">
+                <Card.Body>
+                  <div class="row">
+                    <div class="col-3">
+                      <Image src="/penjual1.png" alt="penjual1"className="seller_img rounded-3"></Image>
+                    </div>
+                    <div class="col">
+                      <Card.Title>Nama Penjual</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted kota">Kota</Card.Subtitle>
+                    </div>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center mt-3">
+            <Col sm={6}>
+              <Card className="card-description">
+                <Card.Body>
+                  <Card.Title>Deskripsi</Card.Title>
+                  <Card.Text className="deskripsi">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </Card.Text>
+                  <Card.Text className="deskripsi">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col sm={4}></Col>
+          </Row>
         </Container>
       </div>
 
@@ -133,4 +122,4 @@ function ProductInfo({id}) {
   );
 };
 
-export default ProductInfo;
+export default Product;
