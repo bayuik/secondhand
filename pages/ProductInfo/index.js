80e5d80773fe34e4bc13eb8f4cbccb11731fa817
IlmanNawali <79490380+IlmanNawali@users.noreply.github.com>
2022-07-14 09:49:20 +0700

    Update index.js

--- pages/ProductInfo/index.js
+++ pages/ProductInfo/index.js
@@ -1,125 +1,82 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Col, Row, Image, Form, Navbar, Nav, Card, Container, Button, ListGroup } from "react-bootstrap";
 import { LogoImage } from "../../components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faSearch, faUser, faList, faBell,faArrowRightToBracket
-} from "@fortawesome/free-solid-svg-icons";
-import axios from 'axios';
+import { faSearch, faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import { useState, useEffect } from "react";
-function Product({id}) {
-  const [product, setProducts] = useState([])
+import { Carousel } from "react-responsive-carousel";
 
-  const getProducts = async () => {
-      try{
-          let response = await axios.get("https://api-secondhand-fsw.herokuapp.com/product/2")
-          console.log(response.data.data.products)
-          setProducts(response.data.data.products)
-      }catch(e) {
-          console.log(e.message)
-      }
-  }
+export const getStaticProps = async () => {
+  let response = await axios.get("https://api-secondhand-fsw.herokuapp.com/product");
+  console.log(response.data.data.Products);
+  const data = await response.data.data.Products;
+  return {
+    props: { product: data },
+  };
+};
 
-  useEffect(() => {
-      getProducts();
-  }, [])
+const Home = ({ product }) => {
   return (
     <Row>
-        <div className="invisible-content">
+      <div className="invisible-content">
         <Navbar expand="lg" variant="light" bg="body" fixed="top" className="shadow p-2 mb-5 rounded nav-bar">
-            <Container>
-            <Navbar.Brand href="#"><LogoImage /></Navbar.Brand>
+          <Container>
+            <Navbar.Brand href="#">
+              <LogoImage />
+            </Navbar.Brand>
             <Nav className="me-auto">
-                <Form className="d-flex">
-                <Form.Control type="search" placeholder="Cari di sini..." className="search-box me-auto" aria-label="Search"/>
-                <FontAwesomeIcon icon={faSearch} id="btnIcon" className="" style={{marginTop:"13px", marginLeft:"10px"}}/>
-                </Form>
+              <Form className="d-flex">
+                <div className="search-box">
+                  <Form.Control type="search" placeholder="Cari di sini..." className=" sbox me-auto" aria-label="Search" />
+                  <FontAwesomeIcon icon={faSearch} id="btnIcon" />
+                </div>
+              </Form>
             </Nav>
-            
-            <Button style={{backgroundColor:"#7126B5", borderRadius:'10px'}}>
-                <FontAwesomeIcon icon={faArrowRightToBracket} id="btnIcon" className="" />
-                &nbsp;
-                Masuk
+
+            <Button style={{ backgroundColor: "#7126B5", borderRadius: "10px" }}>
+              <FontAwesomeIcon icon={faArrowRightToBracket} id="btnIcon" className="" />
+              &nbsp; Masuk
             </Button>
-            </Container>
+          </Container>
         </Navbar>
-        </div>
-
-      <div className="spacing mx-auto">
-      <Container >
-          <Row className="justify-content-md-center">
-            <Col sm={6}>
-            <Carousel className="product-img">
-                    <div>
-                      <Image src="/1.png" alt="image1" className="product_img rounded-3"></Image>
-                    </div>
-                    <div>
-                      <Image src="/1.png" alt="image2" className="product_img rounded-3"></Image>
-                    </div>
-                    <div>
-                      <Image src="/1.png" alt="image3" className="product_img rounded-3"></Image>
-                    </div>
-                </Carousel>
-            </Col>
-            <Col sm={4}>
-              <Card className="card-product">
-                <Card.Body>
-                  <Card.Title>Jam Tangan Casio</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Aksesoris</Card.Subtitle>
-                  <Card.Text>
-                    Rp. 250.000
-                  </Card.Text>
-                  <div className="d-grid gap-2 mt-4">
-                  <Button className="text-white purple-bg custom-rounded p-2" type="button">Saya tertarik dan ingin nego</Button>
-                  </div>
-                </Card.Body>
-              </Card>
-
-              <Card className="mt-3 card-size card-product">
-                <Card.Body>
-                  <div className="row">
-                    <div className="col-3">
-                      <Image src="/penjual1.png" alt="penjual1"className="seller_img rounded-3"></Image>
-                    </div>
-                    <div className="col">
-                      <Card.Title>Nama Penjual</Card.Title>
-                      <Card.Subtitle className="mb-2 text-muted kota">Kota</Card.Subtitle>
-                    </div>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-          <Row className="justify-content-md-center mt-3">
-            <Col sm={6}>
-              <Card className="card-description">
-                <Card.Body>
-                  <Card.Title>Deskripsi</Card.Title>
-                  <Card.Text className="deskripsi">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </Card.Text>
-                  <Card.Text className="deskripsi">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col sm={4}></Col>
-          </Row>
-        </Container>
       </div>
-
+      <br />
+      <br />
+      <br />
+      <div>
+        <Carousel className="product-img">
+          <div>
+            <Image src="/banner.png" alt="image1" className="product_img rounded-3" style={{ width: "700px", height: "200px" }}></Image>
+          </div>
+          <div>
+            <Image src="/banner.png" alt="image2" className="product_img rounded-3" style={{ width: "700px", height: "200px" }}></Image>
+          </div>
+          <div>
+            <Image src="/banner.png" alt="image3" className="product_img rounded-3" style={{ width: "700px", height: "200px" }}></Image>
+          </div>
+        </Carousel>
+      </div>
+      <Row className="justify-content-md-left">
+        {product.map(({id, product_name, product_photo, category, price}) => {
+          return (
+            <Col md={1} style={{ margin: "50px" }} key={id}>
+              <Card style={{ width: "200px" }}>
+                <Card.Link href={`/ProductInfo/${id}`}>
+                  <Card.Img variant="top" src={`https://api-secondhand-fsw.herokuapp.com/uploads/${product_photo}`} style={{ height: "150px" }} />
+                  <Card.Body>
+                    <Card.Title>{product_name}</Card.Title>
+                    <Card.Text style={{ fontSize: "13px", color: "#8A8A8A" }}>{category}</Card.Text>
+                    <Card.Text>Rp.{price.toLocaleString()}</Card.Text>
+                  </Card.Body>
+                </Card.Link>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
     </Row>
   );
 };
 
-export default Product;
+export default Home;
